0f20668cc047d92aec125f3cacbea5ce4c938c20
DesperateMaker <thedesperatemaker@gmail.com>
2020-05-19 20:53:17 +0300

    save function complited

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04
 #
 # Essentials: developer tools, build tools
 #
-RUN apt-get update && apt-get install -y --no-install-recommends \
+RUN apt-get update && apt-get install -y --no-install-recommends --fix-missing \
     git curl vim unzip openssh-client wget \
     build-essential cmake checkinstall gcc tmux \
 #Other requirements
@@ -15,37 +15,40 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 # http://askubuntu.com/questions/351318/changing-symlink-python-to-python3-causes-problems
 RUN apt-get install -y --no-install-recommends python python3-pip python-dev python3-dev && \
     pip3 install --no-cache-dir --upgrade pip setuptools && \
-    echo "alias python='python3'" >> /root/.bash_aliases && \
-    echo "alias pip='pip3'" >> /root/.bash_aliases
+    echo "alias python='python3'"   >> /root/.bash_aliases && \
+    echo "alias pip='pip3'"         >> /root/.bash_aliases
 
 #
 # Science libraries and other common packages
 #
 RUN pip3 --no-cache-dir install \
-    numpy scipy==1.1.0 sklearn scikit-image pandas matplotlib Cython requests
-
-# install torch 1.1 built from cuda 10.0
-#RUN pip3 install torch==1.0.0 -f https://download.pytorch.org/whl/cu100/stable
-RUN pip3 install -U https://download.pytorch.org/whl/cu100/torch-1.0.0-cp36-cp36m-linux_x86_64.whl
-
-COPY requirements.txt /tmp/requirements.txt
-RUN pip3 install -U -r /tmp/requirements.txt
+    numpy scipy==1.1.0 sklearn scikit-image pandas matplotlib Cython requests \
+    cupy==4.1.0 wheel
 
 #set a ROOT directory for the app
 WORKDIR /usr/src/deepsnake/
-
-#copy all the files to the container
-COPY . .
 
 ENV ROOT /usr/src/deepsnake
 ENV CUDA_HOME /usr/local/cuda-10.0
 
 #uncomment for use on Turing arch such as Geforce 2070, 2080 etc.
-ENV CUDA_LAUNCH_BLOCKING=1 
+ENV CUDA_LAUNCH_BLOCKING=1
 
-RUN git clone https://github.com/DesperateMaker/apex.git /usr/src/apex 
-#RUN cd /usr/src/apex/ && python3 setup.py install --cuda_ext --cpp_ext && cd ${ROOT}/lib/csrc/dcn_v2 && python3 setup.py build_ext --inplace
- 
-# && \
-#	cd dcn_v2 && python setup.py build_ext --inplace
-#RUN cd data && ln -s /data kitti
+COPY requirements.txt /tmp/requirements.txt
+RUN pip3 install -U -r /tmp/requirements.txt
+
+#copy all the files to the container
+COPY . .
+
+# install torch 1.1 built from cuda 10.0
+#RUN pip3 install torch==1.0.0 -f https://download.pytorch.org/whl/cu100/stable
+RUN pip3 install -U https://download.pytorch.org/whl/cu100/torch-1.0.0-cp36-cp36m-linux_x86_64.whl && \
+# install apex(lib from nvidia) in the same container
+    git clone https://github.com/DesperateMaker/apex.git /usr/src/apex && cd /usr/src/apex/ # && \
+#    python3 setup.py install --cuda_ext --cpp_ext && \
+# install extentions
+#    cd ${ROOT}/lib/csrc/dcn_v2          && python3 setup.py build_ext --inplace && \
+#    cd ${ROOT}/lib/csrc/extreme_utils   && python3 setup.py build_ext --inplace && \
+#    cd ${ROOT}/lib/csrc/roi_align_layer && python3 setup.py build_ext --inplace
+
+# RUN cd ${ROOT}/data && ln -s /data kitti
--- lib/visualizers/snake.py
+++ lib/visualizers/snake.py
@@ -10,7 +10,18 @@ mean = snake_config.mean
 std = snake_config.std
 
 
+def fig_save(name='', fmt='png'):
+    pwd = os.getcwd()
+    iPath = '/results/'
+    if not os.path.exists(iPath):
+        os.mkdir(iPath)
+    os.chdir(iPath)
+    plt.savefig('{}.{}'.format(name, fmt), fmt='png')
+    os.chdir(pwd)
+
 class Visualizer:
+
+
     def visualize_ex(self, output, batch):
         inp = img_utils.bgr_to_rgb(img_utils.unnormalize_img(batch['inp'][0], mean, std).permute(1, 2, 0))
         ex = output['py']
@@ -18,9 +29,10 @@ class Visualizer:
         ex = ex.detach().cpu().numpy() * snake_config.down_ratio
 
         fig, ax = plt.subplots(1, figsize=(20, 10))
-        fig.tight_layout()
+        fig.imshow(inp)
         ax.axis('off')
-        ax.imshow(inp)
+
+        # ax.imshow(inp)
 
         colors = np.array([
             [31, 119, 180],
@@ -42,7 +54,41 @@ class Visualizer:
             poly = np.append(poly, [poly[0]], axis=0)
             ax.plot(poly[:, 0], poly[:, 1], color=color)
 
-        plt.show()
+        fig_save('1')
+
+    def visualize_summary(self, output, batch):
+        inp = img_utils.bgr_to_rgb(img_utils.unnormalize_img(batch['inp'][0], mean, std).permute(1, 2, 0))
+        box = output['detection'][:, :4].detach().cpu().numpy() * snake_config.down_ratio
+        plt.plot(inp)
+        fig_save('0')
+
+        ct_ind = np.unique(cp_ind)
+        score = score[ct_ind]
+        label = label[ct_ind]
+
+        colors = np.array([
+            [31, 119, 180],
+            [255, 127, 14],
+            [46, 160, 44],
+            [214, 40, 39],
+            [148, 103, 189],
+            [140, 86, 75],
+            [227, 119, 194],
+            [126, 126, 126],
+            [188, 189, 32],
+            [26, 190, 207]
+        ]) / 255.
+        np.random.shuffle(colors)
+        colors = cycle(colors)
+        for i in range(len(ex)):
+            color = next(colors).tolist()
+            poly = ex[i]
+            poly = np.append(poly, [poly[0]], axis=0)
+            ax.plot(poly[:, 0], poly[:, 1], color=color, linewidth=5)
+
+            x_min, y_min, x_max, y_max = box[i]
+            ax.plot([x_min, x_min, x_max, x_max, x_min], [y_min, y_max, y_max, y_min, y_min], color='w', linewidth=0.5)
+        fig_save(ct_ind)
 
     def visualize_training_box(self, output, batch):
         inp = img_utils.bgr_to_rgb(img_utils.unnormalize_img(batch['inp'][0], mean, std).permute(1, 2, 0))
@@ -55,7 +101,6 @@ class Visualizer:
         fig, ax = plt.subplots(1, figsize=(20, 10))
         fig.tight_layout()
         ax.axis('off')
-        ax.imshow(inp)
 
         colors = np.array([
             [31, 119, 180],
@@ -80,9 +125,12 @@ class Visualizer:
             x_min, y_min, x_max, y_max = box[i]
             ax.plot([x_min, x_min, x_max, x_max, x_min], [y_min, y_max, y_max, y_min, y_min], color='w', linewidth=0.5)
 
-        plt.show()
+        #plt.show()
+        fig_save(3)
+
+
 
     def visualize(self, output, batch):
-        # self.visualize_ex(output, batch)
+        self.visualize_ex(output, batch)
         self.visualize_training_box(output, batch)
-
+        self.visualize_summary(output, batch)
